1d210bf1acdc7c9adf2f05866c8ca097e1fe7a21
tosgit1989 <purannaa1989@gmail.com>
2017-02-18 07:09:14 +0900

    記事編集ページにも入力フォームを作成

--- edit.php
+++ edit.php
@@ -9,7 +9,18 @@ require_once ('app.php');
 記事の編集
 
 <!-- コンテンツ -->
-
+<form method="POST" action="exec.php/<?php echo $ArticleId ?>">
+    <div class="form-group">
+        <label for="posted_by"><strong>投稿者</strong></label>
+        <input required="required" class="form-control" placeholder="投稿者を入力" name="posted_by" id="posted_by" type="text" value="<?php echo $article['posted_by'] ?>"><br>
+        <label for="title"><strong>タイトル</strong></label>
+        <input required="required" class="form-control" placeholder="タイトルを入力" name="title" id="title" type="text" value="<?php echo $article['title'] ?>"><br>
+        <label for="comment"><strong>コメント</strong></label>
+        <input required="required" class="form-control" placeholder="コメントを入力" name="comment" id="comment" type="text" value="<?php echo $article['comment'] ?>"><br>
+        <input class="form-control" name="exectype" type="hidden" value="edit">
+    </div>
+    <button type="submit">更新する</button>
+</form>
 
 <!-- フッター -->
 
