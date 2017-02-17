1bbe17bfd654c5ffe5fd1996c0e931b57baeb55d
tosgit1989 <purannaa1989@gmail.com>
2017-02-18 08:47:11 +0900

    記事削除フォームを作成

--- delete.php
+++ delete.php
@@ -9,7 +9,20 @@ require_once ('app.php');
 記事の削除
 
 <!-- コンテンツ -->
-
+<?php
+echo $article['title'];
+echo $article['comment'];
+echo $article['posted_by'];
+echo $article['created_at'];
+echo $article['updated_at'];
+?>
+<p>本当に削除しますか？</p>
+<form method="POST" action="/exec.php/<?php echo $ArticleId ?>">
+    <div class="form-group">
+        <input class="form-control" name="exectype" type="hidden" value="delete">
+    </div>
+    <button type="submit">はい</button>
+</form>
 
 <!-- フッター -->
 
