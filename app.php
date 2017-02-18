6cf2db44f6cd1b9b1f241981002ac76e519569c9
tosgit1989 <purannaa1989@gmail.com>
2017-02-18 09:33:52 +0900

    bootstrapのcssファイルを読み込む処理を記述

--- app.php
+++ app.php
@@ -4,3 +4,6 @@ require_once ('src/Services/Methods.php');
 $dataConnect = new \Services\DataHandler();
 $methods = new \Services\Methods();
 ?>
+<head>
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+</head>
