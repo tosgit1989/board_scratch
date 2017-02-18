0cb2ffea057c9a2b48f2efa347615ca9e6fa1b25
tosgit1989 <purannaa1989@gmail.com>
2017-02-18 16:47:05 +0900

    トップページの記事一覧をbootstrapでパネル化、また投稿がないときには「投稿はありません」と表示するようにした。

--- index.php
+++ index.php
@@ -28,14 +28,24 @@ $articles = $dataConnect->getAll();
                 <div class="bs-docs-section">
 
                     <?php
-                    foreach ($articles as $article) {
-                        echo $article['title'];
-                        echo $article['comment'];
-                        echo $article['posted_by'];
-                        echo $article['created_at'];
-                        echo $article['updated_at'];
-                        echo sprintf('<a href="edit.php/%s">編集</a>', $article['id']);
-                        echo sprintf('<a href="delete.php/%s">削除</a>', $article['id']);
+                    if (count($articles) >= 1) {
+                        foreach ($articles as $article) {
+                            echo '<div class="panel panel-primary">';
+                            echo sprintf('<div class="panel-heading"><strong>%s</strong></div>', $article['title']);
+                            echo sprintf('<div class="panel-body">%s</div>', $article['comment']);
+                            echo '<div class ="panel-footer" style = "height: 55px">';
+                            echo sprintf('<div class="col-xs-3">投稿者: %s</div>', $article['posted_by']);
+                            echo sprintf('<div class="col-xs-3">作成: %s</div>', $article['created_at']);
+                            echo sprintf('<div class="col-xs-3">更新: %s</div>', $article['updated_at']);
+                            echo '<div class="col-xs-3">';
+                            echo sprintf('<a href="edit.php/%s" class="btn btn-primary" style="margin-right: 3px">編集</a>', $article['id']);
+                            echo sprintf('<a href="delete.php/%s" class="btn btn-danger">削除</a>', $article['id']);
+                            echo '</div>';
+                            echo '</div>';
+                            echo '</div>';
+                        }
+                    } else {
+                        echo '<p>投稿はありません。</p>';
                     }
                     ?>
 
