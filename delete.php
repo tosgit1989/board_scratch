370928474c5add31f30a32f851ded89e4c393afd
tosgit1989 <purannaa1989@gmail.com>
2017-02-19 14:38:04 +0900

    削除確認ページの記事をパネル化

--- delete.php
+++ delete.php
@@ -14,11 +14,19 @@ require_once ('app.php');
                 <div class="bs-docs-section">
 
                     <?php
-                    echo $article['title'];
-                    echo $article['comment'];
-                    echo $article['posted_by'];
-                    echo $article['created_at'];
-                    echo $article['updated_at'];
+                    echo '<div class="panel panel-primary">';
+                    echo sprintf('<div class="panel-heading"><strong>%s</strong></div>', $article['title']);
+                    echo sprintf('<div class="panel-body">%s</div>', $article['comment']);
+                    echo '<div class ="panel-footer" style = "height: 55px">';
+                    echo sprintf('<div class="col-xs-3">投稿者: %s</div>', $article['posted_by']);
+                    echo sprintf('<div class="col-xs-3">作成: %s</div>', $article['created_at']);
+                    echo sprintf('<div class="col-xs-3">更新: %s</div>', $article['updated_at']);
+                    echo '<div class="col-xs-3">';
+                    echo sprintf('<a href="edit.php/%s" class="btn btn-primary" style="margin-right: 3px">編集</a>', $article['id']);
+                    echo sprintf('<a href="delete.php/%s" class="btn btn-danger">削除</a>', $article['id']);
+                    echo '</div>';
+                    echo '</div>';
+                    echo '</div>';
                     ?>
                     <p>本当に削除しますか？</p>
                     <form method="POST" action="/exec.php/<?php echo $ArticleId ?>">
