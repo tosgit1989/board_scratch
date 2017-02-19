cb7728c39eeb9779b139b4f1b2acc7e109414e17
tosgit1989 <purannaa1989@gmail.com>
2017-02-19 14:35:30 +0900

    変数名変更

--- index.php
+++ index.php
@@ -16,17 +16,17 @@ require_once ('app.php');
                     <div><a href="new.php" class="btn btn-primary" style="position: fixed; top: 66px; right: 16px">記事を新規作成する</a></div>
                     <?php
                     if (count($articles) >= 1) {
-                        foreach ($articles as $article) {
+                        foreach ($articles as $articleEach) {
                             echo '<div class="panel panel-primary">';
-                            echo sprintf('<div class="panel-heading"><strong>%s</strong></div>', $article['title']);
-                            echo sprintf('<div class="panel-body">%s</div>', $article['comment']);
+                            echo sprintf('<div class="panel-heading"><strong>%s</strong></div>', $articleEach['title']);
+                            echo sprintf('<div class="panel-body">%s</div>', $articleEach['comment']);
                             echo '<div class ="panel-footer" style = "height: 55px">';
-                            echo sprintf('<div class="col-xs-3">投稿者: %s</div>', $article['posted_by']);
-                            echo sprintf('<div class="col-xs-3">作成: %s</div>', $article['created_at']);
-                            echo sprintf('<div class="col-xs-3">更新: %s</div>', $article['updated_at']);
+                            echo sprintf('<div class="col-xs-3">投稿者: %s</div>', $articleEach['posted_by']);
+                            echo sprintf('<div class="col-xs-3">作成: %s</div>', $articleEach['created_at']);
+                            echo sprintf('<div class="col-xs-3">更新: %s</div>', $articleEach['updated_at']);
                             echo '<div class="col-xs-3">';
-                            echo sprintf('<a href="edit.php/%s" class="btn btn-primary" style="margin-right: 3px">編集</a>', $article['id']);
-                            echo sprintf('<a href="delete.php/%s" class="btn btn-danger">削除</a>', $article['id']);
+                            echo sprintf('<a href="edit.php/%s" class="btn btn-primary" style="margin-right: 3px">編集</a>', $articleEach['id']);
+                            echo sprintf('<a href="delete.php/%s" class="btn btn-danger">削除</a>', $articleEach['id']);
                             echo '</div>';
                             echo '</div>';
                             echo '</div>';
