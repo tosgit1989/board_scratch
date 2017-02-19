1a8317f20b0900fc34fb7a4f29b0a1a969bfa20e
tosgit1989 <purannaa1989@gmail.com>
2017-02-19 14:13:36 +0900

    変数名変更

--- exec.php
+++ exec.php
@@ -2,24 +2,22 @@
 require_once ('app.php');
 $ArticleId = $methods->getArticleId($_SERVER['REQUEST_URI']);
 
-
-
 if ($_POST['exectype'] == 'new') {
     // 記事の新規作成時
-    $article['title'] = $_POST['title'];
-    $article['comment'] = $_POST['comment'];
-    $article['posted_by'] = $_POST['posted_by'];
-    $article['updated_at'] = $now = date('Y/m/d H:i:s');
-    $article['created_at'] = $now = date('Y/m/d H:i:s');
-    $dataConnect->insert($article, 'articles');
+    $articleNew['title'] = $_POST['title'];
+    $articleNew['comment'] = $_POST['comment'];
+    $articleNew['posted_by'] = $_POST['posted_by'];
+    $articleNew['updated_at'] = $now = date('Y/m/d H:i:s');
+    $articleNew['created_at'] = $now = date('Y/m/d H:i:s');
+    $dataConnect->insert($articleNew, 'articles');
     header('Location: ../');
 } elseif ($_POST['exectype'] == 'edit') {
     // 記事の更新時
-    $article['title'] = $_POST['title'];
-    $article['comment'] = $_POST['comment'];
-    $article['posted_by'] = $_POST['posted_by'];
-    $article['updated_at'] = $now = date('Y/m/d H:i:s');
-    $dataConnect->update($article, ['id' => $ArticleId], 'articles');
+    $articleUpd['title'] = $_POST['title'];
+    $articleUpd['comment'] = $_POST['comment'];
+    $articleUpd['posted_by'] = $_POST['posted_by'];
+    $articleUpd['updated_at'] = $now = date('Y/m/d H:i:s');
+    $dataConnect->update($articleUpd, ['id' => $ArticleId], 'articles');
     header('Location: ../');
 } elseif ($_POST['exectype'] == 'delete') {
     $dataConnect->delete(['id' => $ArticleId], 'articles');
